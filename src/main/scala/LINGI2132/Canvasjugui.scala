3a56c4a0a2533e57d26b09b467a24d98bce8f60b
bastinjul <julien.bastin@student.uclouvain.be>
2019-05-04 17:31:06 +0200

    rework JGShape draw method + globalComposite and clip

--- src/main/scala/LINGI2132/Canvasjugui.scala
+++ src/main/scala/LINGI2132/Canvasjugui.scala
@@ -1,7 +1,6 @@
 package LINGI2132
 
 import org.scalajs.dom
-import org.scalajs.dom.html.Image
 import org.scalajs.dom.raw.{Event, HTMLImageElement}
 import org.scalajs.dom.{CanvasRenderingContext2D, html}
 
@@ -33,8 +32,13 @@ abstract class JGShape(var x: Double, var y: Double) {
     var strokeWidth : Double = 1.0
     var transparency: Double = 1.0
     var fill : Boolean = false
+    var clip : Boolean = false
     var lineDashOffset : Double = 0
     var lineDash: scala.scalajs.js.Array[Double] = scala.scalajs.js.Array.apply(0, 0)
+    var rotation : Double = 0
+    var scaleX : Double = 1
+    var scaleY : Double = 1
+    var compositeOperation : String = "source-over"
 
     object shadow {
       var shadowOffsetX : Double = 0
@@ -42,14 +46,27 @@ abstract class JGShape(var x: Double, var y: Double) {
       var shadowBlur : Double = 0
       var shadowColor : String = "#000000"
     }
+
+    object transformation {
+      var horizontalScaling : Double = 1
+      var horizontalSkewing : Double = 0
+      var verticalSkewing : Double = 0
+      var verticalScaling : Double = 1
+      var horizontalMoving : Double = 0
+      var verticalMoving : Double = 0
+    }
   }
 
-  def draw(ctx: dom.CanvasRenderingContext2D) : Unit = {
+  def contextSetGlobalParameters(ctx: dom.CanvasRenderingContext2D): Unit ={
+    ctx.globalCompositeOperation = this.parameters.compositeOperation
     ctx.globalAlpha = this.parameters.transparency
     ctx.shadowOffsetX = this.parameters.shadow.shadowOffsetY
     ctx.shadowOffsetY = this.parameters.shadow.shadowOffsetY
     ctx.shadowBlur = this.parameters.shadow.shadowBlur
     ctx.shadowColor = this.parameters.shadow.shadowColor
+  }
+
+  def contextSetSpecificTypeParameters(ctx: dom.CanvasRenderingContext2D): Unit ={
     if(this.parameters.fill) {
       ctx.fillStyle = this.parameters.color
     }
@@ -59,7 +76,46 @@ abstract class JGShape(var x: Double, var y: Double) {
       ctx.strokeStyle = this.parameters.color
       ctx.lineWidth = this.parameters.strokeWidth
     }
+  }
+
+  def contextSetTransformation(ctx: dom.CanvasRenderingContext2D): Unit ={
+    ctx.setTransform(this.parameters.transformation.horizontalScaling, this.parameters.transformation.horizontalSkewing,
+      this.parameters.transformation.verticalSkewing, this.parameters.transformation.verticalScaling,
+      this.parameters.transformation.horizontalMoving, this.parameters.transformation.verticalMoving)
+    ctx.scale(this.parameters.scaleX, this.parameters.scaleY)
+    ctx.rotate(this.parameters.rotation)
+  }
+
+  def contextResetTransformation(ctx: dom.CanvasRenderingContext2D): Unit = {
+    ctx.rotate(-this.parameters.rotation)
+    ctx.scale(1/this.parameters.scaleX, 1/this.parameters.scaleY)
+    ctx.setTransform(1, 0, 0, 1, 0, 0)
+  }
+
+  def applyShape(ctx: dom.CanvasRenderingContext2D): Unit ={
+    if(this.parameters.fill) {
+      ctx.fill()
+    }
+    else if (this.parameters.clip) {
+      ctx.clip()
+    }
+    else {
+      ctx.stroke()
+    }
+  }
+
+  def draw(ctx: dom.CanvasRenderingContext2D) : Unit = {
+    contextSetGlobalParameters(ctx)
+
+    contextSetSpecificTypeParameters(ctx)
+
+    contextSetTransformation(ctx)
+
     drawShape(ctx)
+
+    contextResetTransformation(ctx)
+
+    applyShape(ctx)
   }
 
   def drawShape(ctx: dom.CanvasRenderingContext2D) : Unit
@@ -122,13 +178,57 @@ abstract class JGShape(var x: Double, var y: Double) {
     this.parameters.shadow.shadowColor = color
   }
 
+  def rotate(rotation : Double): Unit = {
+    this.parameters.rotation = rotation
+  }
+
+  def scale(x: Double, y: Double): Unit = {
+    this.parameters.scaleY = y
+    this.parameters.scaleX = x
+  }
+
+  def transform(horizontalScaling: Double, horizontalSkewing: Double, verticalSkewing: Double,
+                verticalScaling: Double, horizontalMoving: Double, verticalMoving: Double): Unit = {
+    this.parameters.transformation.horizontalScaling = horizontalScaling
+    this.parameters.transformation.horizontalSkewing = horizontalSkewing
+    this.parameters.transformation.verticalSkewing = verticalSkewing
+    this.parameters.transformation.verticalScaling = verticalScaling
+    this.parameters.transformation.horizontalMoving = horizontalMoving
+    this.parameters.transformation.verticalMoving = verticalMoving
+  }
+
+  def setGlobalCompositeOperation(typeCompositeOperation : String): Unit ={
+    this.parameters.compositeOperation = typeCompositeOperation
+  }
+
+  def clip(clip : Boolean): Unit ={
+    this.parameters.clip = clip
+  }
+
 }
 
 case class JGRectangle(var xr: Double, var yr: Double, var width: Double, var height: Double) extends JGShape(x = xr, y = yr) {
 
+  override def draw(ctx: CanvasRenderingContext2D): Unit = {
+    contextSetGlobalParameters(ctx)
+
+    contextSetSpecificTypeParameters(ctx)
+
+    contextSetTransformation(ctx)
+
+    drawShape(ctx)
+
+    contextResetTransformation(ctx)
+  }
+
   override def drawShape(ctx: dom.CanvasRenderingContext2D): Unit = {
     if(this.parameters.fill) {
-      ctx.fillRect(x, y, width, height)
+      ctx.fillRect(x, y , width, height)
+    }
+    else if (this.parameters.clip) {
+      ctx.beginPath()
+      ctx.rect(x, y , width, height)
+      ctx.clip()
     }
     else {
       ctx.strokeRect(x, y, width, height)
@@ -143,11 +243,22 @@ case class JGLine(var x1: Double, var y1: Double, var x2: Double, var y2: Double
     setParameters(ctx)
     ctx.moveTo(x1, y1)
     ctx.lineTo(x2, y2)
-    ctx.stroke()
   }
 }
 
 case class JGLinearGradient(var x1: Double, var y1: Double, var x2: Double, var y2: Double, rect: JGRectangle) extends JGShape(x = rect.x, y = rect.y) with ColorStop {
+
+  override def draw(ctx: CanvasRenderingContext2D): Unit = {
+    contextSetGlobalParameters(ctx)
+
+    contextSetSpecificTypeParameters(ctx)
+
+    contextSetTransformation(ctx)
+
+    drawShape(ctx)
+
+    contextResetTransformation(ctx)
+  }
 
   override def drawShape(ctx: CanvasRenderingContext2D): Unit = {
     val linearGradient = ctx.createLinearGradient(x1, y1, x2, y2)
@@ -164,6 +275,19 @@ case class JGLinearGradient(var x1: Double, var y1: Double, var x2: Double, var 
 }
 
 case class JGRadialGradient(var x1: Double, var y1: Double, var r1: Double, var x2: Double, var y2: Double, var r2: Double, rect: JGRectangle) extends JGShape(x = rect.x, y = rect.y) with ColorStop {
+
+  override def draw(ctx: CanvasRenderingContext2D): Unit = {
+    contextSetGlobalParameters(ctx)
+
+    contextSetSpecificTypeParameters(ctx)
+
+    contextSetTransformation(ctx)
+
+    drawShape(ctx)
+
+    contextResetTransformation(ctx)
+  }
+
   override def drawShape(ctx: CanvasRenderingContext2D): Unit = {
     val radialGradient = ctx.createRadialGradient(x1, y1, r1, x2, y2, r2)
     this.colors.colorStop.foreach(c => radialGradient.addColorStop(c._1, c._2))
@@ -218,72 +342,51 @@ trait LineParameters {
 case class JGCircle(var xc: Double, var yc: Double, var radius: Double) extends JGShape(x = xc, y = yc) {
 
   override def drawShape(ctx: dom.CanvasRenderingContext2D): Unit = {
-    if(this.parameters.fill) {
-      ctx.beginPath()
-      ctx.arc(x, y, radius, 0.0, Math.PI * 2, true)
-      ctx.fill()
-
-    }
-    else {
-      ctx.beginPath()
-      ctx.arc(x, y, radius, 0.0, Math.PI * 2, true)
-      ctx.stroke()
-    }
+    ctx.beginPath()
+    ctx.arc(x, y, radius, 0.0, Math.PI * 2, true)
 
   }
 }
 
 case class JGArc(var xa: Double, var ya: Double, var radius: Double, var startingAngle: Double, var endAngle: Double, var anticlockwise: Boolean) extends JGShape(x = xa, y = ya){
   override def drawShape(ctx: CanvasRenderingContext2D): Unit = {
-    if(this.parameters.fill) {
-      ctx.beginPath()
-      ctx.arc(x, y, radius, startingAngle, endAngle, anticlockwise)
-      ctx.fill()
-
-    }
-    else {
-      ctx.beginPath()
-      ctx.arc(x, y, radius, startingAngle, endAngle, anticlockwise)
-      ctx.stroke()
-    }
+    ctx.beginPath()
+    ctx.arc(x, y, radius, startingAngle, endAngle, anticlockwise)
   }
 }
 
 case class JGQuadraticCurve(var cp1x: Double, var cp1y: Double, var xc: Double, var yc: Double) extends JGShape(x = xc, y = yc) {
 
   override def drawShape(ctx: dom.CanvasRenderingContext2D): Unit = {
-    if(this.parameters.fill){
-      ctx.beginPath()
-      ctx.quadraticCurveTo(cp1x, cp1y, x, y)
-      ctx.fill()
-    }
-    else {
-      ctx.beginPath()
-      ctx.quadraticCurveTo(cp1x, cp1y, x, y)
-      ctx.stroke()
-    }
-
+    ctx.beginPath()
+    ctx.quadraticCurveTo(cp1x, cp1y, x, y)
   }
 }
 
 case class JGBezierCurve(var cp1x: Double, var cp1y: Double, var cp2x: Double, var cp2y: Double, var xc: Double, var yc: Double) extends JGShape(x = xc, y = yc) {
 
   override def drawShape(ctx: dom.CanvasRenderingContext2D): Unit = {
-    if(this.parameters.fill){
-      ctx.beginPath()
-      ctx.bezierCurveTo(cp1x, cp1y, cp2x, cp2y, x, y)
-      ctx.fill()
-    }
-    else {
-      ctx.beginPath()
-      ctx.bezierCurveTo(cp1x, cp1y, cp2x, cp2y, x, y)
-      ctx.stroke()
-    }
+    ctx.beginPath()
+    ctx.bezierCurveTo(cp1x, cp1y, cp2x, cp2y, x, y)
   }
 }
 
 case class JGPatterns(var src: String, patternType: String, rect: JGRectangle) extends JGShape(x = rect.x, y = rect.y) {
+
+  override def draw(ctx: CanvasRenderingContext2D): Unit = {
+    contextSetGlobalParameters(ctx)
+
+    contextSetSpecificTypeParameters(ctx)
+
+    contextSetTransformation(ctx)
+
+    drawShape(ctx)
+
+    contextResetTransformation(ctx)
+  }
+
   override def drawShape(ctx: CanvasRenderingContext2D): Unit = {
+    this.parameters.fill = true
     val image = dom.document.createElement("img").asInstanceOf[HTMLImageElement]
     image.src = src
     image.onload = { evt: Event =>
@@ -294,7 +397,72 @@ case class JGPatterns(var src: String, patternType: String, rect: JGRectangle) e
   }
 }
 
+case class JGImage(var src : String, var xi : Double, var yi : Double) extends JGShape(x = xi, y = yi) {
+
+  object slice {
+    var slicing : Boolean = false
+    var sx : Double = 0
+    var sy : Double = 0
+    var sWidth : Double = 0
+    var sHeight : Double = 0
+    var dWidth : Double = 0
+    var dHeight : Double = 0
+  }
+
+  override def draw(ctx: CanvasRenderingContext2D): Unit = {
+    contextSetTransformation(ctx)
+
+    drawShape(ctx)
+
+    contextResetTransformation(ctx)
+  }
+
+  override def drawShape(ctx: CanvasRenderingContext2D): Unit = {
+    val image = dom.document.createElement("img").asInstanceOf[HTMLImageElement]
+    image.src = src
+    image.onload = { evt: Event =>
+      if(this.slice.slicing)
+        ctx.drawImage(image, this.slice.sx, this.slice.sy, this.slice.sWidth, this.slice.sHeight, this.x, this.y, this.slice.dWidth, this.slice.dHeight)
+      else
+        if (this.slice.dWidth > 0 && this.slice.dHeight > 0)
+          ctx.drawImage(image, x, y, this.slice.dWidth, this.slice.dHeight)
+        else
+          ctx.drawImage(image, x, y)
+    }
+  }
+
+  def resize(width: Double, height: Double): Unit = {
+    this.slice.dWidth = width
+    this.slice.dHeight = height
+  }
+
+  def slice(sx: Double, sy: Double, sWidth: Double, sHeight : Double, dx: Double, dy: Double, dWidth: Double, dHeight: Double): Unit ={
+    this.slice.sx = sx
+    this.slice.sy = sy
+    this.slice.sWidth = sWidth
+    this.slice.sHeight = sHeight
+    this.x = dx
+    this.y = dy
+    this.slice.dWidth = dWidth
+    this.slice.dHeight = dHeight
+    this.slice.slicing = true
+  }
+}
+
 case class JGText(var text: String, var xt: Double, var yt: Double) extends JGShape(x = xt, y = yt) with StylingText {
+
+  override def draw(ctx: CanvasRenderingContext2D): Unit = {
+    contextSetGlobalParameters(ctx)
+
+    contextSetSpecificTypeParameters(ctx)
+
+    contextSetTransformation(ctx)
+
+    drawShape(ctx)
+
+    contextResetTransformation(ctx)
+  }
+
   override def drawShape(ctx: CanvasRenderingContext2D): Unit = {
     ctx.font = this.textStyle.font
     ctx.textAlign = this.textStyle.textAlign
@@ -367,6 +535,57 @@ object Extensions {
 
     def transparency(t: Double) : Array[ApplyOn] = {
       s.foreach(sh => sh.transparency(t))
+      s
+    }
+
+    def setLineDash(value: scala.scalajs.js.Array[Double]) : Array[ApplyOn] = {
+      s.foreach(sh => sh.setLineDash(value))
+      s
+    }
+
+    def lineDashOffset(value: Double) : Array[ApplyOn] = {
+      s.foreach(sh => sh.lineDashOffset(value))
+      s
+    }
+
+    def shadowOffsetX(offset : Double): Array[ApplyOn] = {
+      s.foreach(sh => sh.shadowOffsetX(offset))
+      s
+    }
+
+    def shadowOffsetY(offset: Double): Array[ApplyOn] = {
+      s.foreach(sh => sh.shadowOffsetY(offset))
+      s
+    }
+
+    def shadowBlur(blur: Double): Array[ApplyOn] = {
+      s.foreach(sh => sh.shadowBlur(blur))
+      s
+    }
+
+    def shadowColor(color: String): Array[ApplyOn] = {
+      s.foreach(sh => sh.shadowColor(color))
+      s
+    }
+
+    def rotate(rotation : Double): Array[ApplyOn] = {
+      s.foreach(sh => sh.rotate(rotation))
+      s
+    }
+
+    def scale(x: Double, y: Double): Array[ApplyOn] = {
+      s.foreach(sh => sh.scale(x, y))
+      s
+    }
+
+    def transform(horizontalScaling: Double, horizontalSkewing: Double, verticalSkewing: Double,
+                  verticalScaling: Double, horizontalMoving: Double, verticalMoving: Double): Array[ApplyOn] = {
+      s.foreach(sh => sh.transform(horizontalScaling, horizontalSkewing, verticalSkewing, verticalScaling, horizontalMoving, verticalMoving))
+      s
+    }
+
+    def setGlobalCompositeOperation(typeCompositeOperation : String): Array[ApplyOn] ={
+      s.foreach(sh => sh.setGlobalCompositeOperation(typeCompositeOperation))
       s
     }
   }
--- src/main/scala/LINGI2132/CanvasjuguiExample.scala
+++ src/main/scala/LINGI2132/CanvasjuguiExample.scala
@@ -3,14 +3,13 @@ package LINGI2132
 import org.scalajs.dom
 import dom.{document, html}
 import Extensions._
-import org.scalajs.dom.raw.HTMLImageElement
 
 object CanvasjuguiExample {
   def run(canvas: html.Canvas): Unit = {
     val canvasjugui = new Canvasjugui(canvas)
 
     val circles = Array.fill(4)(JGCircle(50, 50, 50))
-    val rectangles = Array.fill(2)(JGRectangle(20, 20, 20, 20))
+    val rectangles = Array.fill(3)(JGRectangle(20, 20, 20, 20))
     val bezierCurves = Array.fill(2)(JGBezierCurve(75, 37, 70, 25, 50, 25))
     val arcs = Array.fill(2)(JGArc(60, 60, 20, 0.0, Math.PI/2, true))
     val line = JGLine(0, 0, 35, 35)
@@ -18,7 +17,11 @@ object CanvasjuguiExample {
     val radialGradient = JGRadialGradient(45, 45, 10, 52, 50, 30, JGRectangle(0, 0, 150, 150))
     val patterns = JGPatterns("https://mdn.mozillademos.org/files/222/Canvas_createpattern.png", "repeat", JGRectangle(0, 300, 150, 150))
     val text = JGText("Hello World!", 400, 20)
+    val image = JGImage("https://mdn.mozillademos.org/files/5397/rhino.jpg", 500, 100)
+    val image2 = JGImage("https://mdn.mozillademos.org/files/5397/rhino.jpg", 500, 200)
+    val clip = JGRectangle(0, 0, 500, 500)
 
+    (circles ++ bezierCurves).setGlobalCompositeOperation("overlay")
     circles(0).translate(20, 20)
     rectangles(0).translateX(10)
     rectangles(1).translateY(5)
@@ -30,11 +33,12 @@ object CanvasjuguiExample {
 
     circles(3).fill(true)
     circles(3).transparency(0.5)
-
     rectangles(1).clearArea(true)
+    rectangles(1).setGlobalCompositeOperation("overlay")
 
     bezierCurves.fill(true)
     bezierCurves.clearArea(true)
+
 
     arcs(1).fill(true)
 
@@ -54,10 +58,23 @@ object CanvasjuguiExample {
     radialGradient.addColorStop(0.9, "#019F62")
     radialGradient.addColorStop(1, "rgba(1, 159, 98, 0)")
     radialGradient.fill(true)
+    radialGradient.setGlobalCompositeOperation("overlay")
 
     text.changeColor("#3FD72F")
     text.font("48px serif")
     text.textBaseline("hanging")
+
+    image.resize(100, 60)
+    image2.slice(33, 71, 104, 124, 500, 200, 87, 104)
+
+    rectangles(0).translate(50, 50)
+    rectangles(0).rotate((Math.PI / 180) * 25)
+
+    rectangles(0).scale(2, 2)
+
+    rectangles(2).transform(2, 0, 0, 2, 30, 30)
+
+    clip.clip(true)
 
     canvasjugui += circles
     canvasjugui += rectangles
@@ -68,6 +85,9 @@ object CanvasjuguiExample {
     canvasjugui += radialGradient
     canvasjugui += patterns
     canvasjugui += text
+    canvasjugui += image
+    canvasjugui += image2
+    //canvasjugui += clip
 
     canvasjugui.draw()
 
